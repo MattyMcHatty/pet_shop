525a36b63549965a408cbe3401701cb8fb9ff611
Matthew Arthur <virtuousvolgin@gmail.com>
2022-09-05 10:20:19 +0100

    Final commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -56,11 +56,8 @@ def add_pet_to_customer(customer, new_pet):
     customer['pets'].append(new_pet)
 
 def customer_can_afford_pet(customer, new_pet):
-    if customer['cash'] >= new_pet['price']:
-        return True
-    else:
-        return False
-
+    return customer['cash'] >= new_pet['price']
+    
 def sell_pet_to_customer(pet_shop, pet, customer):
     if pet == None:
         pass
@@ -68,11 +65,8 @@ def sell_pet_to_customer(pet_shop, pet, customer):
         if customer_can_afford_pet(customer, pet) == False:
             pass
         else:
+            remove_pet_by_name(pet_shop, pet['name'])
             add_pet_to_customer(customer, pet)
             increase_pets_sold(pet_shop, 1)        
             remove_customer_cash(customer, pet['price'])
             add_or_remove_cash(pet_shop, pet['price'])
-
-
-
-
